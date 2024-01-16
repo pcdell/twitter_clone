01468758ce4c4c0e7145e6cbf2f91ac65838eaa2
pcdell <pcdell1001@gmail.com>
2024-01-16 19:41:08 +0000

    atualização

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "tweets.apps.TweetsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'twitter_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'twitter',
+        'USER': 'postgres',
+        'PASSWORD': '1001',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
